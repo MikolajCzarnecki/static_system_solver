3908b7a25869f2fbcc3bcf23c884d099e9fc3e89
MikolajCzarnecki <133150221+MikolajCzarnecki@users.noreply.github.com>
2023-05-24 09:53:11 +0200

    WIP tests for generating constraints from shapes
    tests for generating constraints for scipy.optimize.linprog from shape with verticles as points listed in argument

---
+++ tests/tests_constraints_from_shape_inside.py
@@ -0,0 +1,25 @@
+import py_test
+from numpy.testing import assert_allclose
+
+import numpy as np
+
+from static_system_solver.constraints_from_shape_inside\
+      import constraints_from_shape_inside
+
+# def test_square():
+    #Generate constraints for shape between points:
+    #[[0,0],[1,0],[1,1],[0,1]]
+    
+    # objective = np.array[[],[]]
+
+def test_triangle():
+    #Generate constraints for shape between points:
+    #[[0,0],[1,1],[0.5,2]]
+    
+    shape = np.array([[0,0],[1,0],[1,1],[0,1]])
+    
+    solution = constraints_from_shape_inside(shape)
+
+    true_solution = np.array[[[-4,1],[1,-1],[2,1]],[0,0,3]]
+
+    assert_allclose(solution, true_solution)
